d98a78bf9cb48cecb24a8f352bb27f6472e4194e
Antonio Nogueira <antonio.nogueira@3ds.com>
2021-02-01 10:45:49 +0100

    fix static directory

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static('widget'));
+app.use('/widget', express.static(path.join(__dirname, 'widget')));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
