38570e4409b94fe199f9b02572711330b4aa9eb5
Antonio Nogueira <antonio.nogueira@3ds.com>
2021-02-04 15:27:33 +0100

    integrated with widget onLoad event sequence

--- widget/scripts/main.js
+++ widget/scripts/main.js
@@ -1,24 +1,28 @@
 // CPE Euromed - bootstrap
 function executeInitWidget() {
   // adapted from https://embed.plnkr.co/plunk/Y2cEa3
-  require(
-    // configuration
-    {
-      paths: {
-        // the Vue lib
-        'Vue': 'https://unpkg.com/vue@2.6.12/dist/vue.min'
-      }
+  require(// configuration
+  {
+    paths: {
+      // the Vue lib
+      Vue: "https://unpkg.com/vue@2.6.12/dist/vue.min",
     },
-    // load libs right now
-    ['Vue'],
-    function(Vue)
-    {
-      //console.log("before calling Vue ctor");
-      var myWidgetVarApp = new Vue({
-          el : "#myWidgetApp",
+  }, // load libs right now
+  ["Vue"], function (Vue) {
+    var myWidget = {
+      onLoad: function () {
+        //console.log("before calling Vue ctor");
+        var myWidgetVarApp = new Vue({
+          el: "#myWidgetApp",
           data: {
-              greeting : "Hello Widget from Vue.JS and Bootstrap CSS template!"
-          }});
-      //console.log("after calling Vue ctor");
-    });//require
+            greeting: "Hello Widget from Vue.JS and Bootstrap CSS template!",
+          },
+          created: function () {
+            console.log("VueJs created");
+          },
+        });
+      },
+    };
+    widget.addEvent("onLoad", myWidget.onLoad);
+  }); //require
 }
